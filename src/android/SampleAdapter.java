d0771e310bd778e03d5bb734e93286ba04eff2c0
sgonzalez_acuacar <sgonzalez@acuacar.com>
2020-07-28 22:27:13 -0500

    Se agrego import de Progressbar

--- src/android/SampleAdapter.java
+++ src/android/SampleAdapter.java
@@ -7,6 +7,7 @@ import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import com.bumptech.glide.Glide;
 
